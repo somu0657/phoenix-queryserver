dda637485eda973cbf3d90895d335f805acfee0c
somu0657 <63715061+somu0657@users.noreply.github.com>
2022-09-08 20:40:57 +0530

    Update QueryServerOptions.java

--- phoenix-queryserver/src/main/java/org/apache/phoenix/queryserver/QueryServerOptions.java
+++ phoenix-queryserver/src/main/java/org/apache/phoenix/queryserver/QueryServerOptions.java
@@ -59,7 +59,7 @@ public class QueryServerOptions {
     public static final String DEFAULT_PHOENIX_QUERY_SERVER_CLUSTER_BASE_PATH = "/phoenix";
     public static final String DEFAULT_PHOENIX_QUERY_SERVER_SERVICE_NAME = "queryserver";
     public static final String DEFAULT_PHOENIX_QUERY_SERVER_ZK_ACL_USERNAME = "phoenix";
-    public static final String DEFAULT_PHOENIX_QUERY_SERVER_ZK_ACL_PASSWORD = "phoenix";
+    public static final String DEFAULT_PHOENIX_QUERY_SERVER_ZK_ACL_FAKEPASSWORD = "phoenix";
 
     // Maven repo defaults
     public static final boolean DEFAULT_CLIENT_JARS_ENABLED = false;
@@ -88,7 +88,7 @@ public class QueryServerOptions {
                         .setIfUnset(PHOENIX_QUERY_SERVER_ZK_ACL_USERNAME,
                             DEFAULT_PHOENIX_QUERY_SERVER_ZK_ACL_USERNAME)
                         .setIfUnset(PHOENIX_QUERY_SERVER_ZK_ACL_PASSWORD,
-                            DEFAULT_PHOENIX_QUERY_SERVER_ZK_ACL_PASSWORD);
+                            DEFAULT_PHOENIX_QUERY_SERVER_ZK_ACL_FAKEPASSWORD);
 
         return options;
     }
